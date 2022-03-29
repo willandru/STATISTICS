1d205fdc19b54a987265f40afbcceae2d4b70499
willandru <willandru@hotmail.com>
2022-03-29 17:05:59 -0500

    Studing Confidence INtervals in R¡¡¡¡¡¡

--- Intervals/IntervalosConfianza.R
+++ Intervals/IntervalosConfianza.R
@@ -1,34 +1,32 @@
-# La vida útil de una bebida carbonatada es de interes. 
-#Se seleccionan 10 botellas aleatoriamente para pruebas
-#y se obtienen los siguientes resultados:
+#2.20   Vida útil de las bebidas
 
 vida.util <- c(180, 138, 124, 163, 124, 159, 106, 134, 115, 139)
 
-  #INTERVALO DE CONFIANZA PARA LA MEDIA:
+#INTERVALO DE CONFIANZA PARA LA MEDIA CON VARIANZA DESCONOCIDA
   sample.mean <- mean(vida.util)
   sample.n <- length(vida.util)
-  t.test(vida.util, conf.level=0.99)
-
   sample.sd <- sd(vida.util)
+  e<- sample.sd/sqrt(sample.n)
+  my.quantil <-  (0.995)
   
-b= 1-0.99
- t1<- qt(b/2,sample.n-1)
-e<- sample.sd/sqrt(sample.n)
+  t1<- qt(my.quantil,sample.n-1)
 
-limite.inf <- (sample.mean)-(t1*(e))
-limite.sup <- (sample.mean)+(t1*(e))
+  limite.inf <- (sample.mean)-(t1*(e))
+  limite.sup <- (sample.mean)+(t1*(e))
+  cbind(limite.inf, limite.sup)
+  
+  t.test(vida.util, conf.level= 0.99)
 
-limite.inf
-limite.sup
 
-# INSTRUMENTOS ELECTRÓNICOS
+#2.22 IC 95% PARA LA MEDIA CON VARIANZA DESCONOCIDA
 
 tiempo.reparacion <- c(159, 280, 101, 212, 224, 379, 179, 264, 222, 362
                        , 168, 250, 149, 260, 485, 170)
+
 t.test(tiempo.reparacion, conf.level=0.95)
 
 tiempo.10 <- c(1,2,1,3,5,1,5,2,3,5,3,6,5,3,2,1,6,8,2,3)
 tiempo.20 <- c(7,6,8,9,5,5,9,7,5,4,8,6,6,8,4,5,6,8,7,7)
 
-t.test(tiempo.10, tiempo.20, var.equal=TRUE)
+#t.test(tiempo.10, tiempo.20, var.equal=TRUE)
 t.test(tiempo.10, tiempo.20, var.equal=FALSE)
